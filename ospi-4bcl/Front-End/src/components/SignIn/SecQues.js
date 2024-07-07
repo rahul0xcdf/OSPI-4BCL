1881a3b82376c07e5afe4c187a23edc36f0cb7b4
euphoricair7 <shailjashaktawat04@gmail.com>
2024-07-07 20:15:46 +0530

    Updating signin backend

--- ospi-4bcl/Front-End/src/components/SignIn/SecQues.js
+++ ospi-4bcl/Front-End/src/components/SignIn/SecQues.js
@@ -8,7 +8,7 @@ import "./signIn.css";
 
 const SecQues = ({ setSfa, setTfa,username,password, SQ1,SQ2,SQ3,ans1,ans2,ans3 }) => {
     
-    console.log(SQ1);
+    //console.log(SQ1);
 
     
     const clrscr = ()=>{
@@ -124,100 +124,3 @@ const SecQues = ({ setSfa, setTfa,username,password, SQ1,SQ2,SQ3,ans1,ans2,ans3 
 
 export default SecQues;
 
-
-
-/*
-import React from 'react'
-import {useState} from "react"
-import "./signIn.css"
-
-<<<<<<< HEAD
-</form>const SecQues = ({setSfa,setTfa}) =>{
-=======
-const SecQues = ({setSfa,setTfa,SQ1,SQ2,SQ3,ans1,ans2,ans3}) =>{
->>>>>>> f8aec1b854a969991c9248839c0eb012507e7aab
-
-    const clrscr = ()=>{
-        setSq1Col("");
-        setSq2Col("");
-        setSq3Col("");
-    }
-    const OnSubmitFxn = (event) =>{
-        event.preventDefault();
-        if(sq1Col.toLowerCase() === ans1.toLowerCase() && sq2Col.toLowerCase() === ans2.toLowerCase() && sq3Col.toLowerCase() === ans3.toLowerCase())
-        {
-            setSfa(true);
-            setTfa(false);
-        }
-        else{
-            alert("ERROR! Please enter correct answers.");
-            clrscr();
-        }
-    }
-    const [bdrRadius,setBdrRadius] = useState("0%");
-    const buttonStyle = {
-        borderRadius : bdrRadius,
-    }
-    const OnEnter = () =>{
-        setBdrRadius("20%");
-    }
-    const onLeave = () =>{
-        setBdrRadius("0%");
-    }
-    const [bdrRadius2,setBdrRadius2] = useState("0%");
-    const buttonStyle2 = {
-        borderRadius : bdrRadius2,
-    }
-    const OnEnter2 = () =>{
-        setBdrRadius2("20%");
-    }
-    const onLeave2 = () =>{
-        setBdrRadius2("0%");
-    }
-    
-    const[sq1Col,setSq1Col] = useState("") //creates a state userNameCol
-    const handleSq1 = (event) =>{
-        setSq1Col(event.target.value)
-    }
-    const[sq2Col,setSq2Col] = useState("") //creates a state userNameCol
-    const handleSq2 = (event) =>{
-        setSq2Col(event.target.value)
-    }
-    const[sq3Col,setSq3Col] = useState("") //creates a state userNameCol
-    const handleSq3 = (event) =>{
-        setSq3Col(event.target.value)
-    }
-    
-    const shouldDispClear = (sq1Col.length > 0 || sq2Col.length > 0 || sq3Col.length > 0)
-    return (
-        <div>
-            <h1 align="center">Security Questions</h1>
-            <hr color="black"></hr><br></br>
-            <h2 align = "center"><form onSubmit = {OnSubmitFxn} id = "SecQuesForm">
-                <label>
-                    {SQ1}
-                </label><br></br>
-                <input type = "text" className = "inputBox" value = {sq1Col} onChange = {handleSq1} maxLength = {20} minLength={3}
-                placeholder='Your answer' required/>
-                <br></br><br></br>
-                <label>
-                    {SQ2}
-                </label><br></br>
-                <input type = "text" className = "inputBox" value = {sq2Col} onChange = {handleSq2} maxLength = {20} minLength={3}
-                placeholder='Your answer' required/>
-                <br></br><br></br>
-                <label>
-                    {SQ3}
-                </label><br></br>
-                <input type = "text" className = "inputBox" value = {sq3Col} onChange = {handleSq3} maxLength = {20} minLength={3}
-                placeholder='Your answer' required/>
-                <br></br><br></br>
-                <button type = "submit" className = "buttons" style = {buttonStyle} onMouseEnter={OnEnter} onMouseLeave={onLeave}>Submit</button>
-                <br></br><br></br>{shouldDispClear && <button onClick = {clrscr} className = "buttons" style = {buttonStyle2} onMouseEnter={OnEnter2} onMouseLeave={onLeave2}>Clear</button>}
-            </form></h2>
-        </div>
-    );
-}
-
-export default SecQues;
-*/
--- ospi-4bcl/Front-End/src/components/SignIn/SignIn.js
+++ ospi-4bcl/Front-End/src/components/SignIn/SignIn.js
@@ -64,10 +64,10 @@ const SignIn = ({ setFfa, setSfa, setUsername, setPassword, setSQ1, setSQ2, setS
         <form id="signInForm" onSubmit={handleSignIn}>
         
           <label>Username:</label>
-          <input type="text" className="inputBox" value={username} onChange={(e) => setUsernameCol(e.target.value)} required />
+          <input type="text" className="inputBox" value={username} onChange={(e) => setUsernameCol(e.target.value)} maxLength={20} minLength={3} placeholder='eg "name123"' required />
           <br></br><br></br><br></br>
           <label>Password:</label>
-          <input type="password" className="inputBox" value={password} onChange={(e) => setPasswordCol(e.target.value)} required />
+          <input type="password" className="inputBox" value={password} onChange={(e) => setPasswordCol(e.target.value)} maxLength={20} minLength={3} required autoComplete="off" />
         
           <br></br><br></br>
                     <button type="submit" className="buttons" style={buttonStyle} onMouseEnter={OnEnter} onMouseLeave={onLeave}>Sign-In</button>
@@ -84,110 +84,4 @@ export default SignIn;
 
 
   
-  /*
-
-import React from 'react'
-import {useState} from "react"
-import "./signIn.css"
-
-<<<<<<< HEAD
-
-//react code
-
-const SignIn = ({setFfa, setSfa}) => {
-=======
-const SignIn = ({setFfa, setSfa,setUsnm,setPswrd,setSQ1,setSQ2,setSQ3,setAns1,setAns2,setAns3,setPhNo}) => {
->>>>>>> f8aec1b854a969991c9248839c0eb012507e7aab
-
-    const clrscr = ()=>{
-        setUserNameCol("");
-        setPasswordCol("");
-    }    
-    const OnSubmitFxn = (event) =>{
-        event.preventDefault();
-        const newArr = ["123","123","DefaultQ","DefaultQ","DefaultQ","123","123","123","1234567899"];
-        if(userNameCol === newArr[0] && passwordCol === newArr[1])
-        {
-            setUsnm(newArr[0]);
-            setPswrd(newArr[1]);
-            setSQ1(newArr[2]);
-            setSQ2(newArr[3]);
-            setSQ3(newArr[4]);
-            setAns1(newArr[5]);
-            setAns2(newArr[6]);
-            setAns3(newArr[7]);
-            setPhNo(newArr[8]);
-            setFfa(true);
-            setSfa(false);
-        }
-        else{
-            alert("ERROR! Please enter correct username and password.");
-            clrscr();
-        }
-    }
-    const [bdrRadius,setBdrRadius] = useState("0%");
-   
-    const buttonStyle = {
-        borderRadius : bdrRadius,
-    }
-    const [bdrRadius2,setBdrRadius2] = useState("0%");
-    const buttonStyle2 = {
-        borderRadius : bdrRadius2,
-    }
-    const OnEnter2 = () =>{
-        setBdrRadius2("20%");
-    }
-    const onLeave2 = () =>{
-        setBdrRadius2("0%");
-    }
-    const OnEnter = () =>{
-        setBdrRadius("20%");
-    }
-    const onLeave = () =>{
-        setBdrRadius("0%");
-    }
-    const[userNameCol,setUserNameCol] = useState("") //creates a state userNameCol
-    const handleUserName = (event) =>{
-        setUserNameCol(event.target.value)
-        console.log(userNameCol)
-    }
-    const[passwordCol,setPasswordCol] = useState("") //creates a state passwordCol
-    const handlePassword = (event) =>{
-        setPasswordCol(event.target.value)
-<<<<<<< HEAD
-    }
-
-    
-
-=======
-    }//
-    
-    const shouldDispClear = (userNameCol.length > 0 || passwordCol.length > 0)
->>>>>>> f8aec1b854a969991c9248839c0eb012507e7aab
-    return(
-        <div>
-            <h1 align="center">Sign-In</h1>
-            <hr color="black"></hr>
-            <br></br><br></br>
-            <h2 align = "center">
-                <form id = "signInForm" onSubmit = {OnSubmitFxn}>
-                    <label>Username: </label> 
-                    <input type = "text" className = "inputBox" value = {userNameCol} onChange = {handleUserName} maxLength = {20} minLength={3}
-                placeholder='eg "name123"' required/>
-                    <br></br><br></br><br></br>
-                    <label>Password: </label> 
-                    <input type = "password" className = "inputBox" value = {passwordCol} onChange = {handlePassword} maxLength = {20} minLength={3}
-                    required auto-complete = "off"/>
-                    <br></br><br></br>
-                    <button type = "submit" className = "buttons" style = {buttonStyle} onMouseEnter={OnEnter} onMouseLeave={onLeave}>Sign-In</button>
-                    {shouldDispClear && <div><br></br><br></br><button onClick = {clrscr} className = "buttons" style = {buttonStyle2} onMouseEnter={OnEnter2} onMouseLeave={onLeave2}>Clear</button></div>}
-                </form>
-            </h2><br></br><br></br>
-            <p align = "center">Don't have an account? <a href = "google.com">Create an account</a></p>
-        </div>
-    );
-}
-
-export default SignIn
-
-*/
+ 
